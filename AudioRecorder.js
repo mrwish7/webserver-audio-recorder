f9292c0ca93145d20671768547f15ffd3faa5b75
Adam Wisher <37659188+mrwish7@users.noreply.github.com>
2024-09-03 09:26:21 +0100

    Update version number

--- AudioRecorder.js
+++ AudioRecorder.js
@@ -1,7 +1,7 @@
 // Plugin configuration, this is used in the administration when plugins are loaded
 var pluginConfig = {
     name: 'Audio recorder',
-    version: '1.0',
+    version: '1.1',
     author: 'mrwish7',
     frontEndPath: 'AudioRec/recorder.js'
 }
